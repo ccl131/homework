5a7ed875e5353749a09c9852c1bac1681da24959
氯化碳 <18613807019@163.com>
2021-11-19 19:10:17 +0800

    足球

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //make the camera a certain offset away from the player(the ball) which makes it possible for the camers to
+    //follow the ball when it's in movement
+    public GameObject player ; 
+    private Vector3 offset ; 
+    
+    void Start()
+    {
+        offset = transform.position - player.transform.position ;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset ; 
+    }
+}
